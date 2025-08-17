d9b68a70cdb06b35dbe5e5497b3ff07678a05661
이규연(Lee Kyuyeon) <lmy6268@gmail.com>
2025-08-17 10:48:58 +0900

    [PP-1] 기존 앱 gradle -> Kotlin DSL 로 변환 등 리팩토링

---
+++ app/build.gradle.kts
@@ -0,0 +1,76 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    alias(libs.plugins.propose.android.application)
+    id("com.google.android.gms.oss-licenses-plugin")
+    alias(libs.plugins.propose.android.hilt)
+    alias(libs.plugins.propose.android.application.compose)
+}
+
+android {
+    namespace = "com.hanadulset.pro_poseapp"
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    //opencv모듈과 pytorch 모듈에서 동시에 존재하는 libc++_shared.so 파일 간의 충돌을 막기 위함.
+    packaging {
+        jniLibs.pickFirsts += "**/libc++_shared.so"
+    }
+    val properties = Properties().apply {
+        load(FileInputStream(rootProject.file("keystore.properties")))
+    }
+
+    fun Properties.getValue(key: String) = this[key] as String
+
+
+    signingConfigs {
+        create("release") {
+            storeFile = file(properties.getValue("SIGNED_STORE_FILE"))
+            storePassword = properties.getValue("SIGNED_STORE_PASSWORD")
+            keyAlias = properties.getValue("SIGNED_KEY_ALIAS")
+            keyPassword = properties.getValue("SIGNED_KEY_PASSWORD")
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+    defaultConfig {
+        //OpenCV의 모든 모듈이 아닌, 모바일 전용 모듈만 사용
+        ndk {
+            abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a"))
+        }
+    }
+
+
+}
+
+dependencies {
+    implementation(libs.bundles.android.core)
+    implementation(libs.junit)
+    implementation(libs.bundles.android.test)
+
+    //Paths
+    implementation(projects.data)
+    implementation(projects.domain)
+    implementation(projects.presentation)
+    implementation(projects.utils)
+
+    implementation(libs.facebook.ads.sdk)
+
+
+    //이미지 크롭
+    implementation(libs.vanniktech.imageCropper)
+
+    implementation(libs.androidx.splash)
+    implementation(libs.bundles.aws.s3)
+}
---
+++ build-logic/convention/src/main/kotlin/com/hanadulset/pro_poseapp/build_logic/convention/AppConfigure.kt
@@ -0,0 +1,12 @@
+package com.hanadulset.pro_poseapp.build_logic.convention
+
+object AppConfigure {
+    const val TARGET_SDK = 35
+    const val COMPILE_SDK = 35
+    const val MIN_SDK = 26
+    const val APPLICATION_ID = "com.hanadulset.pro_poseapp"
+    object Version {
+        const val NAME = "1.0.16"
+        const val CODE = 30
+    }
+}
