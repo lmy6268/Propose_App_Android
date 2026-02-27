ad9b8615dfd415668265286e347184f843ee55e0
이규연(Lee Kyuyeon) <lmy6268@gmail.com>
2026-02-27 15:36:09 +0900

    [Refactor] UseCase Facade 패턴 도입 및 폴더 구조 정리

--- domain/src/main/java/com/hanadulset/pro_poseapp/domain/usecase/ai/PreLoadModelUseCase.kt
+++ domain/src/main/java/com/hanadulset/pro_poseapp/domain/usecase/ai/PreLoadModelUseCase.kt
@@ -1,9 +1,8 @@
-package com.hanadulset.pro_poseapp.domain.usecase
+package com.hanadulset.pro_poseapp.domain.usecase.ai
 
 import com.hanadulset.pro_poseapp.domain.repository.ImageRepository
 import javax.inject.Inject
 
 class PreLoadModelUseCase @Inject constructor(private val imageRepository: ImageRepository) {
     suspend operator fun invoke() = imageRepository.preRunModel()
-
 }
